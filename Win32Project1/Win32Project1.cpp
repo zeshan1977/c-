2f56918fb66fb83e028be93f45654314ff5d6ab4
Mohammed Zeshan <khan.mza@gmail.com>
2017-05-22 20:28:07 -0400

    FirstCut

---
+++ Win32Project1/Win32Project1.cpp
@@ -0,0 +1,53 @@
+﻿// Win32Project1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector> // provides STL vector
+#include <string>
+#include "CreditCard.h" // provides CreditCard, cout, string
+using namespace std; // make std accessible
+
+void testCard() { // CreditCard test function
+	vector<CreditCard*> wallet(10); // vector of 10 CreditCard pointers
+	// allocate 3 new cards
+	wallet[0] = new CreditCard("5391 0375 9387 5309", "John Bowman", 2500);
+	wallet[1] = new CreditCard("3485 0399 3395 1954", "John Bowman", 3500);
+	wallet[2] = new CreditCard("6011 4902 3294 2994", "John Bowman", 5000);
+	for (int j = 1; j <= 16; j++) { // make some charges
+		wallet[0]->chargeIt(double(j)); // explicitly cast to double
+		wallet[1]->chargeIt(2 * j); // implicitly cast to double
+		wallet[2]->chargeIt(double(3 * j));
+	}
+	cout << "Card payments:\n";
+	for (int i = 0; i < 3; i++) { // make more charges
+		cout << *wallet[i];
+		while (wallet[i]->getBalance() > 100.0) {
+			wallet[i]->makePayment(100.0);
+			cout << "New balance = " << wallet[i]->getBalance() << "\n";
+		}
+			cout << "\n";
+		delete wallet[i]; // deallocate storage
+	}
+}
+//int _tmain(int argc, _TCHAR* argv[])
+int main()
+{
+	testCard();
+	string s = "abc";
+	string t = "cde";
+	s += s + t[1] + s;
+	cout << "Question R 1.6 String s" << s<<endl;
+
+	double* dp[10];
+
+	
+	for (int i = 0; i < 10; i++) {
+		dp[i] = new double();
+		//dp[i] = 0.0
+	}
+	free(*dp);
+
+//return EXIT_SUCCESS; // successful execution
+
+	return 0;
+}
